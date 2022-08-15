c8b9068015207a34bb8ba606d0437a9b9f1698c4
Paul Louw <pldbrn.117@gmail.com>
2022-08-15 23:20:55 +0200

    Added groovy and jenkins support. Included seperate sourcable bashrc file for dependencies etc.

---
+++ .userconf.sh
@@ -0,0 +1,25 @@
+# Additional bashrc settings, source from bashrc.
+
+# Needed for jenins linter api
+#JENKINS_USER_ID=
+#JENKINS_API_TOKEN=
+#JENKINS_PASSWORD=
+#JENKINS_URL=
+
+function _update_ps1() {
+    PS1=$(powerline-shell $?)
+}
+
+# Langage server 3rd party dependencies.
+# https://github.com/zigtools/zls#installation
+PATH="$PATH:$HOME/zls/"
+# https://github.com/GroovyLanguageServer/groovy-language-server#build
+PATH="$PATH:/opt/groovy-language-server.jar"
+
+if [[ $TERM != linux && ! $PROMPT_COMMAND =~ _update_ps1 ]]; then
+    PROMPT_COMMAND="_update_ps1; $PROMPT_COMMAND"
+fi
+
+source ~/lscolors.sh
+
+export PATH
--- neovim/LinuxVim/luad/nvim/lua/vimplugs.lua
+++ neovim/LinuxVim/luad/nvim/lua/vimplugs.lua
@@ -16,19 +16,17 @@ else
 	use 'roxma/vim-hug-neovim-rpc'
 end
 
+-- Setup env variables for jenkins api, see .userconf.sh
+use({ 'ckipp01/nvim-jenkinsfile-linter', requires = { "nvim-lua/plenary.nvim" } })
+
 use 'neovim/nvim-lspconfig'
 
 use 'deoplete-plugins/deoplete-lsp'
 
--- use {
---     'junegunn/fzf', 
---     run = ':fzf#install'
--- }
--- 
--- use 'junegunn/fzf.vim'
 use 'nvim-lua/plenary.nvim'
 
 use 'ionide/Ionide-vim'
+
 use {
   'nvim-telescope/telescope.nvim', tag = '0.1.0',
 -- or                            , branch = '0.1.x',
@@ -37,17 +35,31 @@ use {
 -- Disables relative numbers when in insertmode:
 use 'jeffkreeftmeijer/vim-numbertoggle'
 
+
 use 'ziglang/zig.vim'
+-- Requires zls installed andin path.
 require'lspconfig'.zls.setup{}
 require'lspconfig'.pyright.setup{}
+-- Requires groovy server see:
+-- :h lspconfig-all
+require'lspconfig'.groovyls.setup{
+    -- Unix
+    cmd = { "java", "-jar", "/opt/groovy-language-server.jar" },
+}
 use {
     'nvim-lualine/lualine.nvim',
     requires = { 'kyazdani42/nvim-web-devicons', opt = true }
   }
+
+--[[
+    LOAD CONFIG FILES
+]]--
 require('plugsconf.telescope_opts')
 require('plugsconf.lspconf')
+
 --require('plugsconf.lualineconf')
 require('plugsconf.lualineconf_bubbles')
 --require('plugsconf.lualineconf_slanted')
 --require('plugsconf.lualineconf_evil')
+
 end)
