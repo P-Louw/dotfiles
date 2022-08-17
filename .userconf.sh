9cfc7176bb30f3f2ea71e52b1ab6b4adccb08bf6
Paul.de.Bruin <paul.de.bruin@topicus.nl>
2022-08-17 10:48:05 +0200

    Added env vars to sourcable bash script.

--- .userconf.sh
+++ .userconf.sh
@@ -15,7 +15,7 @@ function _update_ps1() {
 PATH="$PATH:$HOME/zls/"
 # https://github.com/GroovyLanguageServer/groovy-language-server#build
 PATH="$PATH:/opt/groovy-language-server.jar"
-
+# https://github.com/b-ryan/powerline-shell
 if [[ $TERM != linux && ! $PROMPT_COMMAND =~ _update_ps1 ]]; then
     PROMPT_COMMAND="_update_ps1; $PROMPT_COMMAND"
 fi
