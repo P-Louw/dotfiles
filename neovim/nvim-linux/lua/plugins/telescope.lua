ca1e8503558a88c0f6dc5786999b384f11f1dcd3
P-Louw <pfwlouw@gmail.com>
2026-02-04 08:37:28 +0100

    fix(keybindings): Set search bindings set
      Structure:
      - Used keys table for lazy-loading (plugin loads only when needed)
      - Matches LazyVim's exact descriptions and options
    
      Quick Access (most used):
      - <leader><space> - Find Files (quick access) ✨
      - <leader>, - Switch Buffer (with MRU sorting)
      - <leader>/ - Grep content in files
    
      Find Files (<leader>f) - Searches by filename:
      - <leader>ff - All files (root dir)
      - <leader>fF - All files (current directory)
      - <leader>fg - Git-tracked files only (now correct! ✅)
      - <leader>fr - Recent files
      - <leader>fc - Find nvim config files
    
      Search Content (<leader>s) - Searches inside files:
      - <leader>sg/sG - Grep in root/cwd
      - <leader>sw/sW - Search word under cursor
      - Visual mode: Select text and press <leader>sw to search for it
      - Plus: diagnostics, help, keymaps, commands, marks, etc.
    
      Git:
      - Moved git commits to <leader>gc (from <leader>fc)
      - Git status at <leader>gs
    
      Bonus Features Added:
      - Command history, help pages, keymaps browser, diagnostics, LSP symbols, and more!

--- neovim/nvim-linux/lua/plugins/telescope.lua
+++ neovim/nvim-linux/lua/plugins/telescope.lua
@@ -1,15 +1,95 @@
 return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
-      dependencies = { 'nvim-lua/plenary.nvim' },
-      config = function()
-      require("telescope").setup()
-        -- set keymaps
-        local keymap = vim.keymap
+  "nvim-telescope/telescope.nvim",
+  tag = "0.1.8",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  cmd = "Telescope",
+  keys = {
+    -- Quick access
+    { "<leader>,", "<cmd>Telescope buffers sort_mru=true sort_lastused=true<cr>", desc = "Switch Buffer" },
+    { "<leader>/", "<cmd>Telescope live_grep<cr>", desc = "Grep (Root Dir)" },
+    { "<leader><space>", "<cmd>Telescope find_files<cr>", desc = "Find Files (Root Dir)" },
 
-        keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
-        keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { desc = "Fuzzy find recent files" })
-        keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { desc = "Find string in cwd" })
-        keymap.set("n", "<leader>fs", "<cmd>Telescope git_status<cr>", { desc = "Find string under cursor in cwd" })
-        keymap.set("n", "<leader>fc", "<cmd>Telescope git commits<cr>", { desc = "Find todos" })
-    end,
-    }
+    -- Find files (<leader>f)
+    { "<leader>fb", "<cmd>Telescope buffers sort_mru=true sort_lastused=true<cr>", desc = "Buffers" },
+    { "<leader>fc", "<cmd>Telescope find_files cwd=~/.config/nvim<cr>", desc = "Find Config File" },
+    { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files (Root Dir)" },
+    { "<leader>fF", "<cmd>Telescope find_files cwd=%:p:h<cr>", desc = "Find Files (cwd)" },
+    { "<leader>fg", "<cmd>Telescope git_files<cr>", desc = "Find Files (git-files)" },
+    { "<leader>fr", "<cmd>Telescope oldfiles<cr>", desc = "Recent" },
+    { "<leader>fR", "<cmd>Telescope oldfiles cwd=%:p:h<cr>", desc = "Recent (cwd)" },
+
+    -- Git (<leader>g)
+    { "<leader>gc", "<cmd>Telescope git_commits<cr>", desc = "Commits" },
+    { "<leader>gs", "<cmd>Telescope git_status<cr>", desc = "Status" },
+
+    -- Search (<leader>s)
+    { '<leader>s"', "<cmd>Telescope registers<cr>", desc = "Registers" },
+    { "<leader>sa", "<cmd>Telescope autocommands<cr>", desc = "Auto Commands" },
+    { "<leader>sb", "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "Buffer" },
+    { "<leader>sc", "<cmd>Telescope command_history<cr>", desc = "Command History" },
+    { "<leader>sC", "<cmd>Telescope commands<cr>", desc = "Commands" },
+    { "<leader>sd", "<cmd>Telescope diagnostics bufnr=0<cr>", desc = "Document Diagnostics" },
+    { "<leader>sD", "<cmd>Telescope diagnostics<cr>", desc = "Workspace Diagnostics" },
+    { "<leader>sg", "<cmd>Telescope live_grep<cr>", desc = "Grep (Root Dir)" },
+    { "<leader>sG", "<cmd>Telescope live_grep cwd=%:p:h<cr>", desc = "Grep (cwd)" },
+    { "<leader>sh", "<cmd>Telescope help_tags<cr>", desc = "Help Pages" },
+    { "<leader>sH", "<cmd>Telescope highlights<cr>", desc = "Search Highlight Groups" },
+    { "<leader>sj", "<cmd>Telescope jumplist<cr>", desc = "Jumplist" },
+    { "<leader>sk", "<cmd>Telescope keymaps<cr>", desc = "Key Maps" },
+    { "<leader>sl", "<cmd>Telescope loclist<cr>", desc = "Location List" },
+    { "<leader>sM", "<cmd>Telescope man_pages<cr>", desc = "Man Pages" },
+    { "<leader>sm", "<cmd>Telescope marks<cr>", desc = "Jump to Mark" },
+    { "<leader>so", "<cmd>Telescope vim_options<cr>", desc = "Options" },
+    { "<leader>sR", "<cmd>Telescope resume<cr>", desc = "Resume" },
+    { "<leader>sq", "<cmd>Telescope quickfix<cr>", desc = "Quickfix List" },
+    { "<leader>sw", "<cmd>Telescope grep_string word_match=-w<cr>", desc = "Word (Root Dir)" },
+    { "<leader>sW", "<cmd>Telescope grep_string cwd=%:p:h word_match=-w<cr>", desc = "Word (cwd)" },
+    { "<leader>sw", "<cmd>Telescope grep_string<cr>", mode = "v", desc = "Selection (Root Dir)" },
+    { "<leader>sW", "<cmd>Telescope grep_string cwd=%:p:h<cr>", mode = "v", desc = "Selection (cwd)" },
+    { "<leader>uC", "<cmd>Telescope colorscheme enable_preview=true<cr>", desc = "Colorscheme with Preview" },
+    {
+      "<leader>ss",
+      function()
+        require("telescope.builtin").lsp_document_symbols()
+      end,
+      desc = "Goto Symbol",
+    },
+    {
+      "<leader>sS",
+      function()
+        require("telescope.builtin").lsp_dynamic_workspace_symbols()
+      end,
+      desc = "Goto Symbol (Workspace)",
+    },
+  },
+  config = function()
+    require("telescope").setup({
+      defaults = {
+        prompt_prefix = " ",
+        selection_caret = " ",
+        -- Configure for better integration
+        mappings = {
+          i = {
+            ["<C-Down>"] = function(...)
+              return require("telescope.actions").cycle_history_next(...)
+            end,
+            ["<C-Up>"] = function(...)
+              return require("telescope.actions").cycle_history_prev(...)
+            end,
+            ["<C-f>"] = function(...)
+              return require("telescope.actions").preview_scrolling_down(...)
+            end,
+            ["<C-b>"] = function(...)
+              return require("telescope.actions").preview_scrolling_up(...)
+            end,
+          },
+          n = {
+            ["q"] = function(...)
+              return require("telescope.actions").close(...)
+            end,
+          },
+        },
+      },
+    })
+  end,
+}
