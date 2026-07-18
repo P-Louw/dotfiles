175436be76a8a607ac069269d339bbef8676d665
Paul Louw <paullouw@proton.me>
2026-07-18 14:52:52 +0200

    fix(treesitter): buffer settings

--- neovim/nvim-linux/lua/plugins/treesitter.lua
+++ neovim/nvim-linux/lua/plugins/treesitter.lua
@@ -1,60 +1,59 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    version = false,
-    event = { "BufReadPre", "BufNewFile" },
-    build = ":TSUpdate",
-    dependencies = {
-        "windwp/nvim-ts-autotag",
-    },
-    config = function()
-        local treesitter = require("nvim-treesitter.configs")
+	"nvim-treesitter/nvim-treesitter",
+	version = false,
+	event = { "BufReadPre", "BufNewFile" },
+	build = ":TSUpdate",
+	dependencies = {
+		"windwp/nvim-ts-autotag",
+	},
+	config = function()
+		local treesitter = require("nvim-treesitter.configs")
 
-        treesitter.setup({
-            highlight = {
-                enable = true,
-                additional_vim_regex_highlighting = false,
-            },
-            indent = { enable = true },
-            autotag = {
-                enable = true,
-            },
-            ensure_installed = {
-                "json",
-                "javascript",
-                "typescript",
-                "tsx",
-                "yaml",
-                "html",
-                "css",
-                "scss",
-                "markdown",
-                "markdown_inline",
-                "bash",
-                "lua",
-                "vim",
-                "regex",
-                "dockerfile",
-                "gitignore",
-                "c",
-                "rust",
-                "python",
-                "zig",
-                "latex",
-                "norg",
-                "svelte",
-                "typst",
-                "vue",
-                "helm",
-            },
-            incremental_selection = {
-                enable = true,
-                keymaps = {
-                    init_selection = "<C-space>",
-                    node_incremental = "<C-space>",
-                    scope_incremental = false,
-                    node_decremental = "<bs>",
-                },
-            },
-        })
-    end,
+		treesitter.setup({
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = false,
+			},
+			indent = { enable = true },
+			autotag = {
+				enable = true,
+			},
+			ensure_installed = {
+				"json",
+				"javascript",
+				"typescript",
+				"tsx",
+				"yaml",
+				"html",
+				"css",
+				"scss",
+				"markdown",
+				"markdown_inline",
+				"bash",
+				"lua",
+				"vim",
+				"regex",
+				"dockerfile",
+				"gitignore",
+				"c",
+				"rust",
+				"python",
+				"zig",
+				"norg",
+				"svelte",
+				"typst",
+				"vue",
+				"helm",
+			},
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "<C-space>",
+					node_incremental = "<C-space>",
+					scope_incremental = false,
+					node_decremental = "<bs>",
+				},
+			},
+		})
+	end,
 }
